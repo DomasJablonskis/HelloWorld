ab7de6a2193351c1856135c29b26660b1100e8d9
Domas <domas.jablonskis@gmail.com>
2020-11-21 14:48:25 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/domas/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.domas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
